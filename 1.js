2c09a414e6cf5e3d645fae35745ec36ad843bc5e
thiago <thiago.k01@aluno.ifsc.edu.br>
2026-09-23 10:52:33 -0300

    msg

---
+++ 1.js
@@ -0,0 +1,11 @@
+console.log("alou");
+// Declarar variaveis em JS.
+i=0; //variavel global var
+var nome="Thiago" //define variavel global
+nome=1;
+let numero; //define variavel local por bloco
+const numeroPI = 3.141516;
+
+console.log("Variavl i:"+typeof(i))
+console.log("Variavel nome:"+typeof(nome))
+
